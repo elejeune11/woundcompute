37fcfb8b9b5aee909901ac1d2d035b2552992190
elejeune11 <52667645+elejeune11@users.noreply.github.com>
2022-08-30 16:32:41 -0400

    started adding basic functions and tests

--- src/woundcompute/image_analysis.py
+++ src/woundcompute/image_analysis.py
@@ -1,4 +1,30 @@
+import numpy as np
+from scipy import ndimage
+from skimage.filters import threshold_otsu
+from typing import Union
 
-def fcn_1(x , y):
-    return x + y
+
+def apply_median_filter(array: np.ndarray, filter_size: int) -> np.ndarray:
+    """Given an image array. Will return the median filter applied by scipy"""
+    filtered_array = ndimage.median_filter(array, filter_size)
+    return filtered_array
+
+
+def apply_gaussian_filter(array: np.ndarray, filter_size: int) -> np.ndarray:
+    """Given an image array. Will return the gaussian filter applied by scipy"""
+    filtered_array = ndimage.gaussian_filter(array, filter_size)
+    return filtered_array
+
+
+def compute_otsu_thresh(array: np.ndarray) -> Union[float, int]:
+    """Given an image array. Will return the otsu threshold applied by skimage."""
+    thresh = threshold_otsu(array)
+    return thresh
+
+
+def apply_otsu_thresh(array: np.ndarray) -> np.ndarray:
+    """Given an image array. Will return a boolean numpy array with an otsu threshold applied."""
+    thresh = compute_otsu_thresh(array)
+    thresh_img = array > thresh
+    return thresh_img
 
--- tests/test_image_analysis.py
+++ tests/test_image_analysis.py
@@ -1,11 +1,59 @@
+import numpy as np
+from scipy import ndimage
 from woundcompute import image_analysis as ia
-import pytest
 
 
-def test_fcn_1():
-    x = 7
-    y = 3
-    known = 10
-    found = ia.fcn_1(x , y)
-    assert known == found
+def test_apply_median_filter():
+    array = np.ones((10, 10))
+    array[1, 5] = 10
+    array[7, 3] = 10
+    filter_size = 3
+    known = np.ones((10, 10))
+    found = ia.apply_median_filter(array, filter_size)
+    assert np.all(known == found)
+
+
+def test_apply_gaussian_filter():
+    array = np.ones((10, 10))
+    array[1, 5] = 10
+    array[7, 3] = 10
+    filter_size = 3
+    known = ndimage.gaussian_filter(array, filter_size)
+    found = ia.apply_gaussian_filter(array,filter_size)
+    assert np.all(known == found)
+
+
+def test_compute_otsu_thresh():
+    dim = 10
+    known_lower = 10
+    known_upper = 100
+    std_lower = 2
+    std_upper = 10
+    select = 0.8
+    x1 = np.random.normal(known_lower, std_lower, dim * dim * dim)
+    x1 = np.reshape(x1, (dim, dim, dim))
+    x2 = np.random.normal(known_upper, std_upper, dim * dim * dim)
+    x2 = np.reshape(x2, (dim, dim, dim))
+    choose = np.random.random((dim, dim, dim)) > select
+    x1[choose] = x1[choose] + x2[choose]
+    found = ia.compute_otsu_thresh(x1)
+    assert found > known_lower and found < (known_upper + known_lower)
+
+
+def test_apply_otsu_thresh():
+    dim = 10
+    known_lower = 10
+    known_upper = 10000
+    std_lower = 0.1
+    std_upper = 10
+    select = 0.8
+    x1 = np.random.normal(known_lower, std_lower, dim * dim * dim)
+    x1 = np.reshape(x1, (dim, dim, dim))
+    x2 = np.random.normal(known_upper, std_upper, dim * dim * dim)
+    x2 = np.reshape(x2, (dim, dim, dim))
+    choose = np.random.random((dim, dim, dim)) > select
+    x1[choose] = x1[choose] + x2[choose]
+    known = x1 > np.mean(x1)
+    found = ia.apply_otsu_thresh(x1)
+    assert np.all(known == found)
 
