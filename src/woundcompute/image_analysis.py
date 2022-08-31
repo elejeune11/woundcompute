e7e7d28b71912a962994d3dc20941ef5a82035de
elejeune11 <52667645+elejeune11@users.noreply.github.com>
2022-08-30 21:13:24 -0400

    added functions and tests

--- src/woundcompute/image_analysis.py
+++ src/woundcompute/image_analysis.py
@@ -36,3 +36,78 @@ def get_region_props(array: np.ndarray) -> List:
     region_props = regionprops(label_image)
     return region_props
 
+
+def get_largest_regions(region_props: List, num_regions: int = 3) -> List:
+    """Given a list of region properties. Will return a list of the 3 largest regions.
+    If there are fewer than 3 regions, will return all regions."""
+    area_list = []
+    for region in region_props:
+        area_list.append(region.area)
+    ranked = np.argsort(area_list)[::-1]
+    num_to_return = np.min([len(ranked),num_regions])
+    regions_list = []
+    for kk in range(0, num_to_return):
+        idx = ranked[kk]
+        regions_list.append(region_props[idx])
+    return regions_list
+
+
+def get_domain_center(array: np.ndarray) -> Union[int,float]:
+    """Given an array. Will return center (ix_0, ix_1)"""
+    center_0 = array.shape[0] / 2.0
+    center_1 = array.shape[1] / 2.0
+    return center_0, center_1
+
+
+def compute_distance(
+    a0: Union[int, float],
+    a1: Union[int, float], 
+    b0: Union[int, float],
+    b1: Union[int, float]
+) -> Union[int, float]:
+    """Given two points. Will return distance between them."""
+    dist = ((a0 - b0)**2.0 + (a1 - b1)**2.0)**0.5
+    return dist
+
+
+def get_closest_region(
+    regions_list: List,
+    loc_0: Union[int, float],
+    loc_1: Union[int, float]
+) -> object:
+    """Given a list of region properties. Will return the object closest to location."""
+    center_dist = []
+    for region in regions_list:
+        centroid = region.centroid
+        region_0 = centroid[0]
+        region_1 = centroid[1]
+        dist = compute_distance(region_0, region_1, loc_0, loc_1)
+        center_dist.append(dist)
+    ix = np.argmin(center_dist)
+    return regions_list[ix]
+
+
+def extract_region_props(region_props: object) -> float:
+    """Given region properties from skimage.measure.regionprops.
+    Will return the values of relevant properties.
+    See: https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.regionprops
+    """
+    area = region_props.area
+    axis_major_length = region_props.axis_major_length
+    axis_minor_length = region_props.axis_minor_length
+    centroid = region_props.centroid
+    centroid_row = centroid[0]
+    centroid_col = centroid[1]
+    coords = region_props.coords
+    return area, axis_major_length, axis_minor_length, centroid_row, centroid_col, coords
+
+# def coords_to_mask():
+#     return True
+
+
+# def mask_to_contour():
+#     return True
+
+
+# def mask_to_area():
+#     return True
--- tests/test_image_analysis.py
+++ tests/test_image_analysis.py
@@ -71,3 +71,79 @@ def test_get_region_props():
     region_props = ia.get_region_props(array)
     assert region_props[0].area == np.sum(disk_1)
     assert region_props[1].area == np.sum(disk_2)
+
+
+def test_get_largest_regions():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    rad_2 = 3
+    disk_2 = morphology.disk(rad_2, dtype=bool)
+    rad_3 = 2
+    disk_3 = morphology.disk(rad_3, dtype=bool)
+    dim = 30
+    array = np.zeros((dim, dim))
+    array[0:disk_1.shape[0], 0:disk_1.shape[1]] = disk_1
+    array[-disk_2.shape[0]:, -disk_2.shape[1]:] = disk_2
+    array[0:disk_3.shape[0], -disk_3.shape[1]:] = disk_3
+    region_props = ia.get_region_props(array)
+    num_regions = 2
+    regions_list = ia.get_largest_regions(region_props, num_regions)
+    assert len(regions_list) == 2
+    assert regions_list[0].area == np.sum(disk_1)
+    assert regions_list[1].area == np.sum(disk_2)
+
+
+def test_get_domain_center():
+    array = np.ones((10, 20))
+    center_0, center_1 = ia.get_domain_center(array)
+    assert center_0 == 5
+    assert center_1 == 10
+
+
+def test_compute_distance():
+    a0 = 0
+    a1 = 0
+    b0 = 10
+    b1 = 0
+    known = 10
+    found = ia.compute_distance(a0, a1, b0, b1)
+    assert known == found
+
+
+def test_get_closest_region():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    rad_2 = 3
+    disk_2 = morphology.disk(rad_2, dtype=bool)
+    rad_3 = 2
+    disk_3 = morphology.disk(rad_3, dtype=bool)
+    dim = 30
+    array = np.zeros((dim, dim))
+    array[0:disk_1.shape[0], 0:disk_1.shape[1]] = disk_1
+    array[-disk_2.shape[0]:, -disk_2.shape[1]:] = disk_2
+    array[0:disk_3.shape[0], -disk_3.shape[1]:] = disk_3
+    region_props = ia.get_region_props(array)
+    num_regions = 3
+    regions_list = ia.get_largest_regions(region_props, num_regions)
+    loc_0 = 0
+    loc_1 = 0
+    region = ia.get_closest_region(regions_list, loc_0, loc_1)
+    assert region.area == np.sum(disk_1)
+
+
+def test_extract_region_props():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    region_props = ia.get_region_props(disk_1)
+    region = region_props[0]
+    area, axis_major_length, axis_minor_length, centroid_row, centroid_col, coords = ia.extract_region_props(region)
+    assert area == np.sum(disk_1)
+    assert axis_major_length > 10
+    assert axis_major_length < 11
+    assert axis_minor_length > 10
+    assert axis_minor_length < 11
+    assert centroid_row == 5
+    assert centroid_col == 5
+    assert coords.shape[0] == np.sum(disk_1)
+
+
